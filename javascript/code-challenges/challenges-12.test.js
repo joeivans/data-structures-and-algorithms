f23e73d18a2f49fbab7d0be3a38a5f0d90307c60
Joe Ivans <joseph.ivans@gmail.com>
2021-09-01 08:27:46 -0700

    Challenge 12-2 implementation

--- javascript/code-challenges/challenges-12.test.js
+++ javascript/code-challenges/challenges-12.test.js
@@ -17,9 +17,7 @@ Write a function named validatePin that uses a regular expression pattern to val
 If the PIN is four numerical digits long, return true. Otherwise, return false.
 ------------------------------------------------------------------------------------------------ */
 
-const validatePin = (pin) => {
-  // Solution code here...
-};
+const validatePin = pin => ('' + pin).search(/^\d{4}$/) > -1;
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 3
