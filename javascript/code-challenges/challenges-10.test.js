bc22e2b516c8af8fe1e5c14812b0b09bee30affd
Joe Ivans <joseph.ivans@gmail.com>
2021-09-04 22:10:22 -0700

    Challenge 10-5 implementation

--- javascript/code-challenges/challenges-10.test.js
+++ javascript/code-challenges/challenges-10.test.js
@@ -105,16 +105,20 @@ const grandTotal = stores => {
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 5
 
-Pat has decided that he would also like to organize his data as objects containing the number of cookies sold per hour and the time.
+Pat has decided that he would also like to organize his data as objects containing the number of
+cookies sold per hour and the time.
 
 Here is sample data for the 9:00 sales: { sales: '88 cookies', time: '9 a.m.' }.
 
-Write a function named salesData that uses forEach to iterate over the hourlySales array and create an object for each hour. Return an array of the formatted data.
+Write a function named salesData that uses forEach to iterate over the hourlySales array and create
+an object for each hour. Return an array of the formatted data.
 ------------------------------------------------------------------------------------------------ */
 
-const salesData = (hours, data) => {
-  // Solution code here...
-};
+const salesData = (hours, data) => hours.map((hour, hoursIndex) => ({
+  sales: `${data[hoursIndex]} cookies`,
+  time: hours[hoursIndex],
+}));
+
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 6
