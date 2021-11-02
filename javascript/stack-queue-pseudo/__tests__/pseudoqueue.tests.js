8daf81f592bfb10e36edf03545a41c9e89f8a36d
Joe Ivans <joseph.ivans@gmail.com>
2021-11-01 22:43:43 -0700

    Add scaffolding for challenge
    - Adds the files and folders for the challenge

---
+++ javascript/stack-queue-pseudo/__tests__/pseudoqueue.tests.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const PseudoQueue = require('../index');
+
+describe('PseudoQueue Tests', function () {
+  it('should ..', function () {
+    // Given
+    const queue = new PseudoQueue();
+
+    // When
+
+    // Then
+  });
+});
---
+++ javascript/stack-queue-pseudo/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+class PseudoQueue {
+}
+
+module.exports = PseudoQueue;
