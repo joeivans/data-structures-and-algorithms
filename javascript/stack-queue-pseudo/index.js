6b37b9f9f2b64358d9db8ccdf30752146e010b85
Joe Ivans <joseph.ivans@gmail.com>
2021-11-01 23:26:34 -0700

    Add PseudoQueue implementation
    - Adds enqueue method
    - Adds dequeue method
    - Adds #forward private stack property
    - Adds #backward private stack property
    - Adds size property

--- javascript/stack-queue-pseudo/index.js
+++ javascript/stack-queue-pseudo/index.js
@@ -1,6 +1,31 @@
 'use strict';
 
 class PseudoQueue {
+  #forward = [];
+  #backward = [];
+  size = 0;
+
+  enqueue(data) {
+    const newForward = [];
+    const newBackward = [];
+
+    let _data = this.#forward.pop();
+    while (_data) {
+
+      _data = this.#forward.pop();
+    }
+
+    this.#forward = newForward;
+    this.#backward = newBackward;
+    this.size++;
+  }
+
+  dequeue() {
+    const result = this.#forward[0];
+    this.size--;
+
+    return result;
+  }
 }
 
 module.exports = PseudoQueue;
